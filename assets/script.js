ce119df6f81e0f3282635f8f4b1809c91c7308ed
Junel <robin.lennuj@gmail.com>
2023-05-20 23:58:28 -0700

    Slowly working on JS.

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+  // Quiz questions and answers
+  var questions = [
+    {
+      question: "?",
+      options: [""],
+      answer: "",
+    },
+    {
+      question: "?",
+      options: [""],
+      answer: "",
+    },
+    // Add more questions here
+  ];
+
+
+  // Place Arrays & DOM
+
+  // Place the main functions
