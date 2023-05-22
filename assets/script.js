939ab90ce9358d4790146de14958d1f8b59c7ef5
Junel <robin.lennuj@gmail.com>
2023-05-21 17:30:54 -0700

    Big progress on html, css & js.

--- assets/script.js
+++ assets/script.js
@@ -13,10 +13,24 @@
     // Add more questions here
   ];
 
+
 var currentQuestionIndex = 0;
 var timeLeft = 60;
 var timerInterval;
 
-  // Place Variables, Arrays & DOM
+
+// These variables store references to various HTML elements in the document using their corresponding IDs.
+var startButton = document.getElementById("start-quiz");
+var quizContainer = document.getElementById("quiz-container");
+var questionsElement = document.getElementById("questions");
+var choicesContainer = document.getElementById("choices");
+var timeElement = document.getElementById("time");
+var scoreForm = document.getElementById("score-form");
+var initialsInput = document.getElementById("initials");
+var submitScoreButton = document.getElementById("submit-score");
+var highScoresElement = document.getElementById("high-scores");
+var scoreList = document.getElementById("score-list");
+var clearScores = document.getElementById("clear-scores");
+var startNewGame = document.getElementById("start-new-game");
 
   // Place the main functions
